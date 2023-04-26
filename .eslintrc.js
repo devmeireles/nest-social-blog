b01e50c22dece170f2784356fc0f300e877337d1
Gabriel Santos Meireles <dev.meireles@gmail.com>
2023-04-26 15:45:59 +0100

    adding importer order

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', "eslint-plugin-import-helpers"],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,29 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "no-use-before-define": ["off"],
+    "@typescript-eslint/no-use-before-define": "off",
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        "newlinesBetween": "always",
+        "groups": [
+          "/@nestjs.*/",
+          "/typeorm.*/",
+          "/@shared.*/",
+          [
+            "module"
+          ],
+          [
+            "absolute"
+          ]
+        ],
+        "alphabetize": {
+          "order": "asc",
+          "ignoreCase": true
+        }
+      }
+    ]
   },
 };
